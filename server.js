1c601862a988a9156fb41f31c565f448e4f5f034
Onur Kantar <onur.kantar@ceng.deu.edu.tr>
2020-06-09 21:33:19 +0300

    postinstall package.json edited

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
